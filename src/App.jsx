d4c6b0bef391090220d73f0fb5b4397ff6d1e9a8
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2025-08-05 16:19:47 -0300

    CSS Stylesheets To Style Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import MyComponent from "./components/MyComponent";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <>
       <div className="App">
+        <MyComponent />
         <Todo />
       </div>
     </>
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+import "../css/style.css";
+
+export default function MyComponent() {
+  return (
+    <div className="header">
+      <h1>Todo List</h1>
+    </div>
+  );
+}
