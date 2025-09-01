79c4267cb864c5bbc6435dba721f854ad460f185
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2025-09-01 00:08:32 -0300

    Styling todo items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,9 @@
 import TodoItem from "./TodoItem";
+import styles from "./TodoList.module.css";
 
 export default function TodoList({ todos }) {
   return (
-    <div>
+    <div className={styles.list}>
       {todos.map((item) => (
         <TodoItem key={item} item={item} />
       ))}
